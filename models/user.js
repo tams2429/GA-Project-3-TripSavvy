1f9c3f2543e11e1a84765db56b02056dd06cae86
Eliza Thompson <Eliza_thompson@icloud.com>
2020-08-22 16:19:38 +0100

    fixing unsave

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
 userSchema 
   .set('toJSON', {
     transform(doc, json) {
-      delete json.password
+      delete json.passwords
       return json
     }
   })

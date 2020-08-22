229b6fd9bb7e10c0b27964759eb7cd6954b78344
dnirns <daniel.irons@gmail.com>
2020-08-22 15:21:51 +0100

    responsive nav & home component tweak w/img placeholders

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -8,31 +8,63 @@ class Home extends React.Component {
     return (
       <section className="section">
         <div className="container">
-          <div className="HomePage">
-            <div className="Title">
-              <h1>HOME</h1>
+
+          <div className="columns home-row1">
+
+            <div className="column">
+              <img src="https://www.fillmurray.com/350/250"></img>
             </div>
-            <div className="Categories">
-              <div className="Category1">
-                <img></img>
-              </div>
-              <div className="Category2">
-                <img></img>
-              </div>
-              <div className="Category3">
-                <img></img>
-              </div>
-              <div className="Category4">
-                <img></img>
-              </div>
-              <div className="Category5">
-                <img></img>
-              </div>
-              <div className="Category6">
-                <img></img>
-              </div>
+
+            <div className="column">
+              <img src="https://www.fillmurray.com/350/250"></img>
             </div>
+
+            <div className="column">
+              <img src="https://www.fillmurray.com/350/250"></img>
+            </div>
+
           </div>
+
+          <div className="columns home-row2">
+
+            <div className="column">
+              <img src="https://www.fillmurray.com/350/250"></img>
+            </div>
+
+            <div className="column">
+              <img src="https://www.fillmurray.com/350/250"></img>
+            </div>
+
+            <div className="column">
+              <img src="https://www.fillmurray.com/350/250"></img>
+            </div>
+
+          </div>
+
+
+          {/* <div className="Title">
+            <h1>HOME</h1>
+          </div>
+          <div className="Categories">
+            <div className="Category1">
+              <img></img>
+            </div>
+            <div className="Category2">
+              <img></img>
+            </div>
+            <div className="Category3">
+              <img></img>
+            </div>
+            <div className="Category4">
+              <img></img>
+            </div>
+            <div className="Category5">
+              <img></img>
+            </div>
+            <div className="Category6">
+              <img></img>
+            </div>
+          </div>   */}
         </div>
       </section>
     )

1848ef1b2452ebe226955ed180eb7c5a969ef2d5
dnirns <daniel.irons@gmail.com>
2020-08-23 17:18:13 +0100

    added profile.js and routed into app.js

---
+++ frontend/src/users/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+class Profile extends React.Component {
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+ 
+          <figure className="image is-128x128">
+            <img className="is-rounded" src="http://www.fillmurray.com/200/200" />
+          </figure>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Profile
+
+

6cfeeed9943582dd4e56797f37114c5c808abc4a
dnirns <daniel.irons@gmail.com>
2020-08-24 15:32:18 +0100

    new insomnia

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,16 @@
 const User = require('../models/user')
 const { notFound } = require('../lib/errorMessage')
 
-// ? The URL for this profile route will just be /profile
+//  The URL for this profile route will just be /profile
 
 async function userProfile(req, res, next) {
   try {
-    // ? we find the users profile, by using the ID of the user making the request, so to do this, a user must use a valid token
-    const user = await User.findById(req.currentUser._id).populate('wishlistedCities') // ? populating the virtual "createdQueens" field while finding them, see the user model for how this was created
-    if (!user) throw new Error(notFound) // ? <-- if for any reason that cant be found, throw a not found error
-    res.status(200).json(user) // ? <-- if it is found, send that user object back in the response
+    //  we find the users profile, by using the ID of the user making the request, so to do this, a user must use a valid token
+    const user = await User.findById(req.currentUser._id).populate('wishlistedCities') //  populating the virtual "createdQueens" field while finding them, see the user model for how this was created
+    if (!user) throw new Error(notFound) //  <-- if for any reason that cant be found, throw a not found error
+    res.status(200).json(user) //  <-- if it is found, send that user object back in the response
   } catch (err) {
-    next(err) // ? <-- any error that gets thrown will be sent to the error handler
+    next(err) //  <-- any error that gets thrown will be sent to the error handler
   }
 }
 

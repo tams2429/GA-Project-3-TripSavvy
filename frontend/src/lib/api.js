d57a285c401bcd92f373a8512dd4e9930b09b573
Siu Kei Tam <tam.s.k.92@gmail.com>
2020-08-25 10:34:15 +0100

    Updated api.js and added auth.js for FE requests

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,49 @@
 import axios from 'axios'
+import { getToken } from './auth'
+
+const withHeaders = () => {
+  return {
+    headers: { Authorization: `Bearer ${getToken()}` }
+  }
+}
+
+//* Login and Register Requests
+export const register = formData => {
+  return axios.post('/api/register', formData)
+}
+
+export const login = formData => {
+  return axios.post('/api/login', formData)
+}
 
 
+//* Home Page Requests
 export const getAllCities = () => {
   return axios.get('/api/cities')
 }
+
+//* CityCard Page Requests
+export const getSingleCity = (id) => {
+  return axios.get(`/api/cities/${id}`)
+}
+
+export const editCity = (formData, id) => {
+  return axios.put(`/api/cities/${id}`, formData, withHeaders())
+}
+
+export const deleteCity = (id) => {
+  return axios.delete(`/api/cities/${id}`, withHeaders())
+}
+
+//* Profile Page Requests
+export const getProfile = (id) => {
+  return axios.get(`/api/profile/${id}`, withHeaders())
+}
+
+//* Create a City Page Request
+export const createCity = formData => {
+  return axios.post('/api/cities', formData, withHeaders())
+}
+
+
+
---
+++ frontend/src/lib/auth.js
@@ -0,0 +1,26 @@
+export const setToken = receivedToken => {
+  window.localStorage.setItem('token', receivedToken)
+}
+export const getToken = () => {
+  return window.localStorage.getItem('token')
+}
+export const logout = () => {
+  window.localStorage.removeItem('token')
+}
+const getPayload = () => {
+  const token = getToken()
+  if (!token) return false
+  const parts = token.split('.')
+  if (parts.length < 3) return false
+  return JSON.parse(window.atob(parts[1]))
+}
+export const isAuthenticated = () => {
+  const payload = getPayload()
+  if (!payload) return false
+  const now = Math.round(Date.now() / 1000)
+  return now < payload.exp
+}
+export const isOwner = userId => {
+  if (!isAuthenticated) return false
+  return userId === getPayload().sub
+}

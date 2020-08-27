417ac96bff83a1086405ec99aa7ab4723b8f599e
Eliza Thompson <Eliza_thompson@icloud.com>
2020-08-27 10:54:32 +0100

    cities added

--- db/data/cities.js
+++ db/data/cities.js
@@ -17,15 +17,6 @@ module.exports = [
     cityLatLng: [47.49801, 19.03991],
     cityImg: 'https://www.nationalgeographic.com/content/dam/travel/Guide-Pages/europe/budapest-travel.ngsversion.1502478020304.adapt.1900.1.jpg'
   },
-
-  {
-    name: 'New York',
-    country: 'USA',
-    description: 'the Big Apple. Nice cheesecakes 🍰',
-    categories: ['night', 'culture','food'],
-    cityLatLng: [40.730610, -73.935242],
-    cityImg: 'https://nypost.com/wp-content/uploads/sites/2/2017/11/tax-reform-ny.jpg?quality=90&strip=all&w=1200'
-  },
   
   {
     name: 'Birmingham',
@@ -34,5 +25,284 @@ module.exports = [
     categories: ['food'],
     cityLatLng: [52.489471, -1.898575],
     cityImg: 'https://i2-prod.birminghammail.co.uk/incoming/article14950849.ece/ALTERNATES/s1200/0_DILSHAD-Wednesbury-1.jpg'
+  },
+
+  {
+    name: 'Berlin',
+    country: 'Germany',
+    description: 'Weiners and Techno clubs only for for cool people',
+    categories: ['night', 'culture', 'food'],
+    cityLatLng: [52.5200, 13.4050],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Aerial_view_of_Berlin_%2832881394137%29.jpg/500px-Aerial_view_of_Berlin_%2832881394137%29.jpg'
+  },
+
+  {
+    name: 'Rome',
+    country: 'Italy',
+    description: 'Pizza, culture, pizza, shopping, pizza',
+    categories: ['night', 'culture', 'food'],
+    cityLatLng: [41.9028, 12.4964],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Rome_Montage_2017.png/540px-Rome_Montage_2017.png'
+  },
+
+  {
+    name: 'Chamonix',
+    country: 'France',
+    description: 'Chamonix is one of the oldest ski resorts in France. Great food. Great Apres.',
+    categories: ['night', 'food', 'nature', 'snow'],
+    cityLatLng: [45.9237, 6.8694],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Chamonix_valley_from_la_Fl%C3%A9g%C3%A8re%2C2010_07.JPG/540px-Chamonix_valley_from_la_Fl%C3%A9g%C3%A8re%2C2010_07.JPG'
+  },
+
+  {
+    name: 'Zermatt',
+    country: 'Switzerland',
+    description: 'Snitzel and Skiing',
+    categories: ['night', 'culture', 'food', 'nature', 'snow'],
+    cityLatLng: [46.0207, 7.7491],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/3802_-_Zermatt_-_Matterhorn_viewed_from_Gornergratbahn.JPG/530px-3802_-_Zermatt_-_Matterhorn_viewed_from_Gornergratbahn.JPG'
+  },
+
+  {
+    name: 'Saint Petersburg',
+    country: 'Russia',
+    description: 'Saint Petersburg is historically and culturally associated with the birth of the Russian Empire and Russia\'s entry into modern history as a European great power.',
+    categories: ['night', 'culture', 'food', 'snow'],
+    cityLatLng: [59.9311, 30.3609],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/532px-Peter_%26_Paul_fortress_in_SPB_03.jpg'
+  },
+
+  {
+    name: 'Ibiza',
+    country: 'Spain',
+    description: 'Beaches, Beers, cocktails and Clubs',
+    categories: ['beach', 'night', 'food'],
+    cityLatLng: [39.0200, 1.4821],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Wikipedia_space_ibiza%2803%29.jpg/440px-Wikipedia_space_ibiza%2803%29.jpg'
+  },
+
+  {
+    name: 'Bercelona',
+    country: 'Spain',
+    description: 'Beaches, beautiful buildings and sangria what more could you want',
+    categories: ['beach', 'night', 'culture', 'food', 'nature', 'snow'],
+    cityLatLng: [41.3851, 2.1734],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fbarcelona-beach-umbrella-spain-europe-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Nice',
+    country: 'France',
+    description: 'Great food, great drinks beautiful people, beautiful beaches',
+    categories: ['beach', 'night', 'food'],
+    cityLatLng: [43.7102, 7.2620],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fnice-france-french-riviera-beach-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Split',
+    country: 'Croatia',
+    description: 'The medieval port city’s mild weather is ideal for travelers interested in sailing, and travelers can easily find quiet, isolated beaches on the islands scattered around the Dalmation Coast.',
+    categories: ['beach', 'night', 'culture', 'food'],
+    cityLatLng: [45.1000, 15.2000],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fbeach-split-croatia-palm-trees-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Copenhagen',
+    country: 'Denmark',
+    description: 'Travelers loved biking along Copenhagen’s sandy beaches and waterfront parks.',
+    categories: ['beach', 'night', 'culture', 'food', 'nature'],
+    cityLatLng: [55.6761, 12.5683],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fcopenhagen-amager-beach-denmark-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Dubrovnik',
+    country: 'Crotia',
+    description: 'One of the best beaches in all of Croatia, Dubrovnik’s popular Banje Beach is close to the city’s Old Town, and boasts plenty of beach clubs.',
+    categories: ['beach', 'night', 'culture', 'food', 'nature'],
+    cityLatLng: [42.6507, 18.0944],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fdubrovnik-croatia-dusk-beach-city-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Galway',
+    country: 'Ireland',
+    description: 'The best way to experience County Galway’s distinctly Irish, wild coastline? Spend a weekend at the cozy Dolphin Beach House.',
+    categories: ['beach', 'night', 'culture', 'food', 'nature',
+    cityLatLng: [53.2707, 9.0568],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fgalway-ireland-salthill-beach-ferris-wheel-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Porto',
+    country: 'Portugal',
+    description: 'For a quintessential, sandy beach day, head to the nearby beaches of Praia de Matosinhos or Praia do Ourigo.',
+    categories: ['beach', 'night', 'culture', 'food'],
+    cityLatLng: [41.1579, 8.6291],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fporto-portugal-molhe-beach-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Bodrum',
+    country: 'Turkey',
+    description: 'Clean beaches, lauidback vibes',
+    categories: ['beach', 'food'],
+    cityLatLng: [37.0344, 27.4305],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fbodrum-turkey-wooden-platform-umbrellas-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Dublin',
+    country: 'Ireland',
+    description: 'Yes, Dublin has beaches. Beautiful beaches. Skip crowded Sandycove and head instead to Tower Bay Beach, just 30 minutes north of Dublin City.',
+    categories: ['beach', 'night', 'culture', 'food'],
+    cityLatLng: [53.3498, 6.2603],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fdublin-beach-killiney-ireland-waterfront-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Lisbon',
+    country: 'Portugal',
+    description: 'Heading to one of Lisbon’s beaches? One reader recommended the “lovely beaches” of Cascáis — a wealthy coastal town in the suburbs of Lisbon.',
+    categories: ['beach', 'night', 'culture', 'food'],
+    cityLatLng: [38.7223, 9.1393],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/d/d2/Montagem_de_Lisboa.png'
+  },
+
+  {
+    name: 'Venice',
+    country: 'Italy',
+    description: 'If you’ve maxed out on museums and piazzas, spend an afternoon at Lido Beach. Visitors can take a vaporetto to this seven-mile island in the Venetian Lagoon. Do it right with a negroni in hand.',
+    categories: ['beach', 'night', 'culture', 'food'],
+    cityLatLng: [45.4408, 12.3155],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fvenice-italy-lido-island-boat-water-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Istanbul',
+    country: 'Turkey',
+    description: 'Whether you stay in the city and exploring the markets or head to one of the nearby islands, youre bound to have a wonderful time',
+    categories: ['beach', 'night', 'culture', 'food', 'nature'],
+    cityLatLng: [41.0082, 28.9784],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Istanbul_collage_5j.jpg'
+  },
+
+  {
+    name: 'San Sebastian',
+    country: 'Spain',
+    description: 'Spend hours just strolling the streets window shopping, or walk along the various beaches for a different vibe',
+    categories: ['beach', 'night', 'culture', 'food', 'nature'],
+    cityLatLng: [43.3183, 1.9812],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F04%2Fsan-sebastian-spain-beach-waterfront-BEACHCITY0417.jpg'
+  },
+
+  {
+    name: 'Athens',
+    country: 'Greece',
+    description: 'An intellectual beacon of the ancient world with spectacular Mediterranean landscapes bathed in the famous light, Athens is a sophisticated cosmopolitan hub with delicious gastronomic delights, electrifying nightlife, creative vibes and a booming art scene.',
+    categories: ['beach', 'night', 'culture', 'food', 'nature'],
+    cityLatLng: [37.9838, 23.7275],
+    cityImg: 'https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i719cf880766e6668/version/1580394568/athens-tourism-greece-best-destinations-europe.jpg'
+  },
+
+  {
+    name: 'Vienna',
+    country: 'Austria',
+    description: 'Vienna is history with a capital H, gastronomy, wine, organic and local markets. Vienna is also an incredible architecture with the Hundertwasser Museum, chic restaurants and trendy bars but also amusement parks for young and old. Vienna is classy and fun at the same time.',
+    categories: ['night', 'culture', 'food'],
+    cityLatLng: [48.2082, 16.3738],
+    cityImg: 'https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i8b9e46157d68e99c/version/1580386480/vienna-tourism-austria-best-destinations-europe.jpg'
+  },
+
+  {
+    name: 'Sibiu',
+    country: 'Romania',
+    description: 'There is poetry and beauty on every street corner in Sibiu, and eyes on every roof! Did you know?  This rich city is one of the most visited cities in Romania. Sibiu represents 800 years of history in an unspoilt site, but it is also one of the most modern and dynamic cities in Europe.',
+    categories: ['night', 'culture', 'food', 'nature'],
+    cityLatLng: [45.7936, 24.1213],
+    cityImg: 'https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i78899da832037bf2/version/1580318972/sibiu-tourism-romania-best-destinations-europe.jpg'
+  },
+
+  {
+    name: 'Paris',
+    country: 'France',
+    description: 'Paris is sublime in the spring but it is worth a visit at any time of the year. Culturally unrivaled, Paris is the destination to put on top of your travel bucket list if you like cultural city breaks. The museums of Paris house the greatest masterpieces but its streets, alleys, artist workshops are all to be enjoyed! Picasso, Man Ray, Marcel Duchamp, Brancusi, Yves Klein, Modigliani, but also Molière, Zola, Hugo lived in “the most beautiful city in the world. ',
+    categories: ['night', 'culture', 'food'],
+    cityLatLng: [48.8566, 2.3522],
+    cityImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/550px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg'
+  },
+
+  {
+    name: 'Heviz',
+    country: 'Hungary',
+    description: 'Relaxation and well-being are your key words for a great holiday? Hévíz is the answer.  Did you know that its name "Hévíz" means "hot water" in Hungarian and that the city has the world’s largest natural thermal lake? Imagine a whole city dedicated to relaxation surrounded by lush forests',
+    categories: ['night', 'culture', 'food', 'nature'],
+    cityLatLng: [46.7903, 17.1867],
+    cityImg: 'https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/ie85448dd996dd821/version/1580391425/heviz-tourism-hungary-best-destinations-europe.jpg'
+  },
+
+  {
+    name: 'Cork',
+    country: 'Ireland',
+    description: 'Cork city and county offer visitors history, heritage and landmarks, accompanied by artisan food and drink and a strong cultural, musical and artistic vibe. This is a place of dramatic natural landscapes, riverscapes, harbours, bays and seascapes, a place of mountains, river valleys and coastlines.',
+    categories: ['beach', 'night', 'culture', 'food', 'nature'],
+    cityLatLng: [51.8985, 8.4756],
+    cityImg: 'https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i74c18c840db8d113/version/1580391517/cork-ireland-best-destinations-europe.jpg'
+  },
+
+  {
+    name: 'Menorca',
+    country: 'Spain',
+    description: 'Menorca is the unspoiled island of the Balearic Islands where migratory birds and dolphins are more welcome than large hotel groups. This island is a UNESCO-listed Biosphere Reserve; it is an ideal destination for a stay in the great outdoors, to unwind, discover sublime landscapes and recharge your batteries.',
+    categories: ['beach', 'night', 'culture', 'food', 'nature'],
+    cityLatLng: [39.9496, 4.1104],
+    cityImg: 'https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/ie18e6bba0bb890ee/version/1580394024/menorca-tourism-spain-best-destinations-europe.jpg'
+  },
+
+  {
+    name: 'Florence',
+    country: 'Italy',
+    description: 'The secrets of Tuscany’s most populous city are nearly impossible to unravel in just one trip. Some of its charms are easy to see just walking around: The UNESCO World Heritage designated historic center, with Medici palaces, Renaissance churches, and bridges arching over the Arno. ',
+    categories: ['night', 'culture', 'food', 'nature'],
+    cityLatLng: [43.7696, 11.2558],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F06%2F03%2Fflorence-italy-01-EUROCITYWB20.jpg'
+  },
+
+  {
+    name: 'Seville',
+    country: 'Spain',
+    description: 'This city has everything, though it often feels more like a town, because of the relaxed nature of the people.',
+    categories: ['night', 'culture', 'food'],
+    cityLatLng: [37.3891, 5.9845],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F06%2F03%2Fseville-spain-06-EUROCITYWB20.jpg'
+  },
+
+  {
+    name: 'Krakow',
+    country: 'Poland',
+    description: 'Kraków, like much of Poland, suffered greatly in World War II: Nazis occupied the city and killed tens of thousands. But there were few aerial bombings of Kraków, so much of its historic architecture was spared. In 1978, the old town became one of the first UNESCO World Heritage sites. Now the birthplace of Pope John Paul II is again a thriving city, known in the country as a hub for tech and start-ups — and it’s a must-visit on any Poland itinerary.',
+    categories: ['night', 'culture', 'food'],
+    cityLatLng: [50.0647, 19.9450],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F06%2F03%2Fkrakow-poland-07-EUROCITYWB20.jpg'
+  },
+
+  {
+    name: 'Madrid',
+    country: 'Spain',
+    description: 'A great mix of sights and culture, without being too touristy. And the food is amazing',
+    categories: ['night', 'culture', 'food'],
+    cityLatLng: [40.4168, 3.7038],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F06%2F03%2Fmadrid-spain-10-EUROCITYWB20.jpg'
+  },
+
+  {
+    name: 'Prague',
+    country: 'Czech Republic',
+    description: 'The capital of the Czech Republic, Prague has been a cultural and intellectual hub in Central Europe for centuries. These days, travelers are attracted to its appealing mix of traditional and modern. Want a fascinating tour of a medieval fortress followed by a cutting-edge tasting menu? The city offers both.',
+    categories: ['night', 'culture', 'food'],
+    cityLatLng: [50.0755, 14.4378],
+    cityImg: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F06%2F03%2Fprague-czech-republic-13-EUROCITYWB20.jpg'
   }
 ]
